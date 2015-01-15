80b6fa59730226c66da25f0ad46378d0797e551e
andreluiznsilva <andreluiznsilva@gmail.com>
2015-01-15 10:03:47 -0200

    oracle

--- database/oracle11/XE.sql
+++ database/oracle11/XE.sql
@@ -6,10 +6,4 @@ host /u01/app/oracle/product/11.2.0/xe/bin/orapwd file=/u01/app/oracle/product/1
 @/u01/app/oracle/product/11.2.0/xe/config/scripts/cloneDBCreation.sql
 @/u01/app/oracle/product/11.2.0/xe/config/scripts/postScripts.sql
 @/u01/app/oracle/product/11.2.0/xe/config/scripts/postDBCreation.sql
-
-create user admin identified by admin; 
-grant all privileges to admin; 
-create user dbuser identified by dbuser; 
-grant all privileges to dbuser; 
-
-exit
+@/u01/app/oracle/product/11.2.0/xe/config/scripts/init.sql
