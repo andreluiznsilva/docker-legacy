2841323aaa82198749a953f9bf0a858996f5b3d7
andreluiznsilva <andreluiznsilva@gmail.com>
2015-01-15 10:04:08 -0200

    oracle

---
+++ database/oracle11/init.sql
@@ -0,0 +1,5 @@
+alter user system identified by system;
+create user admin identified by admin; 
+grant all privileges to admin; 
+create user dbuser identified by dbuser; 
+grant all privileges to dbuser;
