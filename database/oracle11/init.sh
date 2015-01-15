e7a5c2e845768d7e780e8213ea4c34ca42cfe73e
andreluiznsilva <andreluiznsilva@gmail.com>
2015-01-15 09:01:50 -0200

    oracle

--- database/oracle11/init.sh
+++ database/oracle11/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+export PATH=$ORACLE_HOME/bin:$PATH
+export ORACLE_SID=XE
+
 sqlplus -S system/oracle@localhost << EOF
 @init.sql
 exit;
