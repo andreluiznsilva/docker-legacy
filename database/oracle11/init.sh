38b9537fd3832c31aa1f329fb021d799a01ee894
andreluiznsilva <andreluiznsilva@gmail.com>
2015-01-15 11:15:33 -0200

    oracle

---
+++ database/oracle11/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+service oracle-xe start
+
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+export PATH=$ORACLE_HOME/bin:$PATH
+export ORACLE_SID=XE
+
+sqlplus -S system/oracle << EOF
+alter user system identified by system;
+create user admin identified by admin; 
+grant all privileges to admin; 
+create user dbuser identified by dbuser; 
+grant all privileges to dbuser;
+exit;
+EOF
