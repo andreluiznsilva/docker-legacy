0b05724c6fbba72d35351d08566c3b2d3e87c507
andreluiznsilva <andreluiznsilva@gmail.com>
2015-05-05 12:37:06 -0300

    apache

--- tools/apache/entrypoint.sh
+++ tools/apache/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$PROXY" ]; then
-		echo -e "<IfModule mod_proxy.c><Proxy *>AddDefaultCharset off\nOrder deny,allow\nAllow from all\n</Proxy>\nProxyRequests On\nProxyRequests On\nProxyPass $PROXY\n</IfModule>" > etc/apache2/mods-enabled/proxy.conf
+		echo -e "<IfModule mod_proxy.c>\n<Proxy *>\nAddDefaultCharset off\nOrder deny,allow\nAllow from all\n</Proxy>\nProxyRequests On\nProxyRequests On\nProxyPass $PROXY\n</IfModule>" > etc/apache2/mods-enabled/proxy.conf
 fi
 
 exec "$@"
