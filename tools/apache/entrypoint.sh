df5d2f81cc6338d8f9c419a8bf4f3a0ca03cb544
andreluiznsilva <andreluiznsilva@gmail.com>
2015-05-05 10:03:11 -0300

    apache

--- tools/apache/entrypoint.sh
+++ tools/apache/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$PROXY" ]; then
-		echo -e "<IfModule mod_proxy.c>\nProxyPass $PROXY\n</IfModule>" > etc/apache2/mods-enabled/proxy.conf
+		echo -e "<IfModule mod_proxy.c>\nProxyRequests On\nProxyRequests On\nProxyPass $PROXY\n</IfModule>" > etc/apache2/mods-enabled/proxy.conf
 fi
 
 exec "$@"
