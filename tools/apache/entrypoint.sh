9425064be76bf389bda2d464949c2e6defaf4979
andreluiznsilva <andreluiznsilva@gmail.com>
2015-05-12 19:06:44 -0300

    docker

--- tools/apache/entrypoint.sh
+++ tools/apache/entrypoint.sh
@@ -5,7 +5,7 @@ HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$HOST_IP dockerhost" >> /etc/hosts
 
 if [ "$PROXY" ]; then
-		echo -e "<IfModule mod_proxy.c>\n<Proxy *>\nAddDefaultCharset off\nOrder deny,allow\nAllow from all\n</Proxy>\nProxyRequests On\nProxyRequests On\nProxyPass $PROXY\n</IfModule>" > etc/apache2/mods-enabled/proxy.conf
+		echo -e "SSLProxyEngine On<IfModule mod_proxy.c>\n<Proxy *>\nAddDefaultCharset off\nOrder deny,allow\nAllow from all\n</Proxy>\nProxyRequests On\nProxyRequests On\nProxyPass $PROXY\n</IfModule>" > etc/apache2/mods-enabled/proxy.conf
 fi
 
 exec "$@"
