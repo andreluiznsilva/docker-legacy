a3b543b14dff48517963e83f78a2a289959e7d03
andreluiznsilva <andreluiznsilva@gmail.com>
2015-06-11 08:46:28 -0300

    aem

---
+++ aem/6.0/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+env JAVA_OPTS="$JAVA_OPTS"
+
+echo "license.product.name=$AEM_LICENSE_PRODUCT_NAME" >> license.properties
+echo "license.customer.name=$AEM_LICENSE_CUSTOMER_NAME" >> license.properties
+echo "license.product.version=$AEM_LICENSE_PRODUCT_VERSION" >> license.properties
+echo "license.downloadID=$AEM_LICENSE_DOWNLOADID" >> license.properties
+
+env JAR_NAME="cq-publish-4503.jar"
+
+if [ "$AEM_MODE" == "author" ]; then
+	env JAR_NAME="cq-author-4502.jar"
+fi
+
+mv AEM_6.0_Quickstart.jar $JAR_NAME.jar
+
+java -jar $JAR_NAME.jar -unpack -r nosamplecontent
+
+exec "$@"
