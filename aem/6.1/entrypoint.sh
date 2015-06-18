bac1b64ebd13dc87baf80293f2bf43f44e92898a
andreluiznsilva <andreluiznsilva@gmail.com>
2015-06-18 10:30:24 -0300

    aem 6.1

---
+++ aem/6.1/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "license.product.name=$AEM_LICENSE_PRODUCT_NAME" >> license.properties
+echo "license.customer.name=$AEM_LICENSE_CUSTOMER_NAME" >> license.properties
+echo "license.product.version=$AEM_LICENSE_PRODUCT_VERSION" >> license.properties
+echo "license.downloadID=$AEM_LICENSE_DOWNLOADID" >> license.properties
+
+export AEM_PORT='4502';
+
+if [ "$AEM_MODE" == "author" ]; then
+	export AEM_PORT='4503';
+fi
+
+java -jar cq-quickstart-beta-6.1.0.jar -r$AEM_MODE -p$AEM_PORT -nobrowser
