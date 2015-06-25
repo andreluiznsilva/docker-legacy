c1a403165082dcfc837530503a028662e2d604b8
andreluiznsilva <andreluiznsilva@gmail.com>
2015-06-25 10:19:16 -0300

    apache

--- apache/2.2/entrypoint.sh
+++ apache/2.2/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 DOCKERHOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$DOCKERHOST_IP dockerhost" >> /etc/hosts
 
-if [ "$PROXY" ]; then
+if [ "$APACHE_MOD_PROXY" ]; then
 	echo -e "$APACHE_MOD_PROXY" > /etc/apache2/mods-enabled/proxy.conf
 fi
 
