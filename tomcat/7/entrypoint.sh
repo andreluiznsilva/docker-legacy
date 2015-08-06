8cd15cd35344a146002f5a4c60e5ed4011466703
andreluiznsilva <andreluiznsilva@gmail.com>
2015-08-06 15:10:57 -0300

    tomcat https

--- tomcat/7/entrypoint.sh
+++ tomcat/7/entrypoint.sh
@@ -5,7 +5,7 @@ env JAVA_OPTS="$JAVA_OPTS"
 
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><tomcat-users><role rolename=\"manager-gui\"/><user username=\"$TOMCAT_USER\" password=\"$TOMCAT_PASSWORD\" roles=\"manager-gui\"/></tomcat-users>" > conf/tomcat-users.xml
 
-sed '88d' $CATALINA_HOME/conf/context.xml && sed '90d' $CATALINA_HOME/conf/context.xml
+sed -i "s#.*</Service>.*#\t<Connector port=\"8443\" protocol=\"org.apache.coyote.http11.Http11Protocol\" maxThreads=\"150\" SSLEnabled=\"true\"	scheme=\"https\" secure=\"true\" clientAuth=\"false\" sslProtocol=\"TLS\" /></Service>#g" $CATALINA_HOME/conf/server.xml
 
 if [ "$TOMCAT_RESOURCES" ]; then
 	sed -i "s#.*<Context>.*#\t<Context>\n$TOMCAT_RESOURCES#g" $CATALINA_HOME/conf/context.xml
