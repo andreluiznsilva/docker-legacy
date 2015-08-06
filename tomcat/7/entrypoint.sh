b1e023f663787ee9fc1f9d1bd8f352c6c48cc138
andreluiznsilva <andreluiznsilva@gmail.com>
2015-08-06 14:24:32 -0300

    tomcat https

--- tomcat/7/entrypoint.sh
+++ tomcat/7/entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 env JAVA_OPTS="$JAVA_OPTS"
 
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><tomcat-users><role rolename=\"manager-gui\"/><user username=\"$TOMCAT_USER\" password=\"$TOMCAT_PASSWORD\" roles=\"manager-gui\"/></tomcat-users>" > conf/tomcat-users.xml && \
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><tomcat-users><role rolename=\"manager-gui\"/><user username=\"$TOMCAT_USER\" password=\"$TOMCAT_PASSWORD\" roles=\"manager-gui\"/></tomcat-users>" > conf/tomcat-users.xml
+
+sed '88d' $CATALINA_HOME/conf/context.xml && sed '90d' $CATALINA_HOME/conf/context.xml
 
 if [ "$TOMCAT_RESOURCES" ]; then
 	sed -i "s#.*<Context>.*#\t<Context>\n$TOMCAT_RESOURCES#g" $CATALINA_HOME/conf/context.xml
